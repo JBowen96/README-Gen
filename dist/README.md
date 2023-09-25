14015b156d610556ddcaeb5552e0f53e312799d4
Josh B <bowenpjoshua@gmail.com>
2023-09-25 10:52:29 -0400

    created files necessary and created the js code.

---
+++ dist/README.md
@@ -0,0 +1,36 @@
+
+    <h1 align="center">README Generator Tool 👋</h1>
+      
+    ![badge](https://img.shields.io/badge/license-ISC-brightgreen)<br />
+    
+    ## Description
+    🔍 This tool asks a series of questions to create a readme.
+    
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Questions](#questions)
+    
+    ## Installation
+    💾 assure that you have inquirer 7.0.0
+    
+    ## Usage
+    💻 To simplify the process of creating README files for various projects.
+    
+    ## License
+    ![badge](https://img.shields.io/badge/license-ISC-brightgreen)
+    <br />
+    This application is covered by the ISC license. 
+    
+    ## Contributing
+    👪 Josh Bowen
+    
+    
+    ## Contact
+    Find me on GitHub: [JBowen96](https://github.com/JBowen96)<br />
+    <br />
+    ✉️ Email me with any questions: name@email.com<br /> 
+    
---
+++ index.js
@@ -0,0 +1,75 @@
+
+//necessary consts for Node
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const genREADME = require("./utils/genREADME");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//user input ReadMe questions 
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projTitle",
+            message: "Give the Title for the Project",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Place project description here: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Installation instructions: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is the usage of this project?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Apply necessary licenses: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Input contributors:"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+}
+
+// util.promisify / async funciton
+async function init() {
+    try {
+        const answer = await promptUser();
+        const genContent = genREADME(answer);
+        await writeFileAsync('./dist/README.md', genContent);
+        console.log('✔️  Successfully wrote to README.md');
+    } catch (err) {
+        console.log(err);
+    }
+}
+init();
